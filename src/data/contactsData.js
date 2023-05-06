4de022b8327e9c04a052e12d18223fd92601eff5
ichrak-harbaoui <61027003+ichrak-harbaoui@users.noreply.github.com>
2023-05-06 18:28:59 +0100

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-  email: "ichrak.harbaoui@esprit.tn",
-  phone: "+216 29 004 449",
-  address: "Tunisia, Ben Arous, Mourouj",
-
-  sheetAPI: "",
-};
--- src/data/socialsData.js
+++
@@ -1,5 +0,0 @@
-export const socialsData = {
-  github: "https://github.com/ichrak-harbaoui",
-  linkedIn: "https://www.linkedin.com/in/ichrak-harbaoui/",
-  gitlab: "https://gitlab.com/ichrak.harbaoui",
-};
